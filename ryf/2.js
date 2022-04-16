3b10548e2823de28d1887b0fd2fffb06e9c97087
Youngzx <1216238955@qq.com>
2022-04-16 17:00:45 +0800

    对象字面量->函数对象(this) 深入理解原型链

---
+++ ryf/2.js
@@ -0,0 +1,20 @@
+let Cat = {
+    name: '',
+    color: ''
+}
+
+let tom = {
+    name: 'Tom',
+    color: 'black'
+}
+
+let cafei = {
+    name: '加菲猫',
+    color: 'brown'
+}
+// 缺点:
+/*
+*1. 写起来麻烦，没有构造函数，可以把对象构造化
+*2. 实例与原型之间没有任何联系
+*
+*/
