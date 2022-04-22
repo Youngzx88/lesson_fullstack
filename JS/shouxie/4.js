ffa7a9d9d0c2c96c021eab2e8a88af9b27aafd1b
Youngzx <1216238955@qq.com>
2022-04-22 08:31:23 +0800

    手写代码

--- JS/shouxie/4.js
+++ JS/shouxie/4.js
@@ -2,11 +2,11 @@ const array = [1,2,3,5,1,5,9,1,2,8];
 // call 摇人指定函数this ->第一个参数
 // call借
 // console.log(typeof array,object.prototype.toString.call(array));
-const uniqueArray = ( arr) =>{
+const uniqueArray = (arr) =>{
     let res = [];
     let map = {};
     // o(n^2)-> o(n)
-    for ( let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         let temp = arr[i];
         if(!map.hasOwnProperty(temp)){
             map[temp] = i;
