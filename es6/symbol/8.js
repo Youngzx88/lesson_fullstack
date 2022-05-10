2f3a3070ee9fb215fd8237641f86a1d1ccd9a5ca
youngzx <1216238955@qq.com>
2022-05-10 16:48:43 +0800

    private

---
+++ es6/symbol/8.js
@@ -0,0 +1,12 @@
+// var sy = sb = Symbol('a');
+
+var PClass = (function(){
+    //私有
+    const a = Symbol('a');//唯一
+    const m = Symbol('m');//唯一
+    function P(){
+        
+    }
+    return P;
+})();
+ 
