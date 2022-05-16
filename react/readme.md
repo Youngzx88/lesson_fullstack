72206ac68a00cd5b15d33e7e98e830819577d1c7
Youngzx <1216238955@qq.com>
2022-05-16 21:53:55 +0800

    react

---
+++ react/readme.md
@@ -0,0 +1,20 @@
+### npm: node package management
+
+#### react创建:
+- npm init @vitejs/app初始化脚手架
+- vitejs当前最快的脚手架
+    1. 输入项目名称
+    2. 确定用react
+    3. 得到了一个项目模板
+#### 下一步
+- cd 项目目录
+- npm i根据项目描述，把项目依赖(dependencies+devDependencies)安装
+- npm run dev
+#### 介绍项目模板
+- 项目模板
+    1. index.html / :3000默认
+        - 挂载点 #root
+        - script引入 src/main.jsx
+            1. src js开发目录
+            2. main.jsx入口文件
+            3. 文件后缀为jsx的时候，react
---
+++ react/try-react/src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import App from './App'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+//react写法
+ReactDOM.render(
+    <div>helloworld!</div>,
+    document.getElementById('root')
+)//在react的dom节点上渲染
+//ES6
+// import Dog from './dog';//引入一个模块 ES6的模块化 从main.js走向其他文件
+// const dog = new Dog();
+// console.log(dog);
+// document.getElementById('root').innerHTML = "hello world"
