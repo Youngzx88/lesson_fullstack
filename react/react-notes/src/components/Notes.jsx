f616959cd441cabeb99f3b6a36c8375586cc0549
Youngzx <1216238955@qq.com>
2022-05-26 21:53:15 +0800

    notes加数据库同步

--- react/react-notes/src/components/Notes.jsx
+++ react/react-notes/src/components/Notes.jsx
@@ -1,37 +1,67 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Note from './Note'
-import Editor from './Editor'
+import { db, loadCollection } from '../database'
 
 const Notes = () => {
     // MVVM
-    const [notes, setNotes] = useState([
-        {body: 'hello~'},
-        {body: '你好~'}
-    ])
+    const [notes, setNotes] = useState([])
     const destroyEntity = (entry) => {
         // console.log(entry)
         // 不是这一项的就留下 filter
         const _entities = notes.filter(
-            item => item.body !== entry.body
+            item => item.$loki !== entry.$loki
         )
         // console.log(_entities)
         setNotes(_entities)
-        // setState
+        // 在数据库中和删除
+        loadCollection('notes')
+            .then((collection)=>{
+                collection.remove(entry);
+                db.saveDatabase();
+            })
     }
     const noteItems = notes.map(
-        (note, index) => 
+        (note) => 
             <Note 
-                key={index}
+                key={note.$loki}
                 entity={note}
                 destroyEntity={destroyEntity}/>
     )
-    const updateEntity = (event) => {
-        console.log(event, '------------')
-    }
+    
 
     const createEntity = () => {
 
     }
+
+    // useEffect()
+    // 先把页面模板显示出来，静态部分先显示出来  
+    //  JSX -> babel + html/css渲染  花时间 组件挂载及渲染时间
+    // 从数据库里去取     无法保证谁先执行完？ 
+    //  useEffect DOMContentLoaded
+    // setNotes  列表出来了？  响应
+    // console.log('|||||||||||||||||')
+    const getInitialData = () => {
+        loadCollection('notes')
+            .then((collection) => {
+                // console.log(collection);
+                // collection.insert([
+                //     {body: 'hello ~'},
+                //     {body: '你好'}
+                // ])
+                const entities = collection.chain()
+                    .find() // select * from orderby...
+                    .simplesort('$loki', 'isdesc')  // 排序 desc 
+                    .data()
+                // console.log(entities)
+                setNotes(entities)
+            })
+
+    }
+    useEffect(() => {
+        // 组件挂载后执行  生命周期
+        // console.log('-------')
+        getInitialData()  // 数据库
+    }, [])
 
     return (
         <div className="ui container notes">
@@ -47,7 +77,6 @@ const Notes = () => {
             <div className="ui divided items">
             {noteItems}
             </div>
-            <Editor updateEntity={updateEntity} />
         </div>
     )
 }
