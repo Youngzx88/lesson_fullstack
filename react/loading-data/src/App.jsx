e92cb16c0e7c2e392e60b6ee2a784bf8c1ac5736
Youngzx <1216238955@qq.com>
2022-05-31 23:13:31 +0800

    fastmock

--- react/loading-data/src/App.jsx
+++ react/loading-data/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [posts,setPosts] = useState([])
   //jsx首屏显示不会受数据请求的阻塞
   useEffect(()=>{
-    console.log('--------');
+    //1.
     // fetch('http://localhost:1234/beers')
     //   .then(data => data.json())
     //   .then(data =>{
@@ -13,13 +13,24 @@ function App() {
     //     setPosts(data);
     //   })
     // },[])
+
+    //2.本地json-server
+    // (async function(){
+    //   const result = await fetch(' http://localhost:1234/beers')
+    //   const data = await result.json();
+    //   // console.log(data);
+    //   setPosts(data);
+    // })()
+
+    //3.远程接口fast mock
     (async function(){
-      const result = await fetch('http://localhost:1234/beers')
-      const data = await result.json();
-      // console.log(data);
-      setPosts(data);
+      const response = await fetch('https://www.fastmock.site/mock/f9755e3a584fdb2f805495e4308be03d/beers/list')
+      const data = await response.json();
+      console.log(data.beers);
+      setPosts(data.beers);
     })()
   },[])
+
   return (
     <div className="App">
       {!posts.length && <div>正在加载中</div>}
