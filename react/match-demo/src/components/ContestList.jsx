8c02afadd4c18e2372669118fb20e49746e9e3cc
youngzx <1216238955@qq.com>
2022-06-13 19:10:29 +0800

    maoyan-demo

---
+++ react/match-demo/src/components/ContestList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import WeUI from 'react-weui'
+import ContestItem from './ContestItem'
+const ContestList = () =>{
+    return (
+        <>
+        <ContestItem/>
+        <ContestItem/>
+        <ContestItem/>
+        </>
+    )
+}
+
+export default ContestList
