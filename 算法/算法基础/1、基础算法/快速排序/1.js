03ac2ea46795942b66c4da2e1530a4729992f386
youngzx <1216238955@qq.com>
2022-06-21 01:24:00 +0800

    快速排序

--- 算法/算法基础/1、基础算法/快速排序/1.js
+++ 算法/算法基础/1、基础算法/快速排序/1.js
@@ -1,22 +1,54 @@
 var q = [4,2,1,6,7,3,0,12,3,2,3,4,2,4,323,4,22,312];
-quick_sort(q,0,q.length-1);
+quickSort(q,0,q.length-1);
 console.log(q);
-function quick_sort(q, l, r)
-{
-    if (l >= r) return;
-
-    var i = l - 1, j = r + 1, x = q[l + r >> 1];
-    while (i < j)
-    {
-        do i ++ ; while (q[i] < x);
-        do j -- ; while (q[j] > x);
-        if (i < j) {
-            var temp;
-            temp = q[i];
-            q[j] = q[i];
-            q[i] = temp
-        };
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    // 定义递归边界，若数组只有一个元素，则没有排序必要
+    if(arr.length > 1) {
+        // lineIndex表示下一次划分左右子数组的索引位
+        const lineIndex = partition(arr, left, right)
+        // 如果左边子数组的长度不小于1，则递归快排这个子数组
+        if(left < lineIndex-1) {
+          // 左子数组以 lineIndex-1 为右边界
+          quickSort(arr, left, lineIndex-1)
+        }
+        // 如果右边子数组的长度不小于1，则递归快排这个子数组
+        if(lineIndex<right) {
+          // 右子数组以 lineIndex 为左边界
+          quickSort(arr, lineIndex, right)
+        }
     }
-    quick_sort(q, l, j),
-    quick_sort(q, j + 1, r);
-}
+    return arr
+  }
+function partition(arr, left, right) {
+    // 基准值默认取中间位置的元素
+    let pivotValue = arr[Math.floor(left + (right-left)/2)]
+    // 初始化左右指针
+    let i = left
+    let j = right
+    // 当左右指针不越界时，循环执行以下逻辑
+    while(i<=j) {
+        // 左指针所指元素若小于基准值，则右移左指针
+        while(arr[i] < pivotValue) {
+            i++
+        }
+        // 右指针所指元素大于基准值，则左移右指针
+        while(arr[j] > pivotValue) {
+            j--
+        }
+  
+        // 若i<=j，则意味着基准值左边存在较大元素或右边存在较小元素，交换两个元素确保左右两侧有序
+        if(i<=j) {
+            swap(arr, i, j)
+            i++
+            j--
+        }
+  
+    }
+    // 返回左指针索引作为下一次划分左右子数组的依据
+    return i
+  }
+  
+  // 快速排序中使用 swap 的地方比较多，我们提取成一个独立的函数
+  function swap(arr, i, j) {
+    [arr[i], arr[j]] = [arr[j], arr[i]]
+  }
