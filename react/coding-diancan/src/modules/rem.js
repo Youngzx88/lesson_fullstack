696e85091ed0ff3774e604397bddae03de667903
youngzx <1216238955@qq.com>
2022-06-21 21:32:47 +0800

    test

--- react/coding-diancan/src/modules/rem.js
+++
@@ -1,8 +0,0 @@
-document.documentElement.style.fontSize =
-    document.documentElement.clientWidth / 3.75 + "px"
-
-// 横竖屏切换
-window.onresize = function() {
-    document.documentElement.style.fontSize =
-        document.documentElement.clientWidth / 3.75 + "px"
-}
--- react/coding-diancan/src/pages/HomeDetail/HomeBussiness/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function HomeBussiness() {
-  return (
-    <div>
-      HomeBussiness
-    </div>
-  )
-}
