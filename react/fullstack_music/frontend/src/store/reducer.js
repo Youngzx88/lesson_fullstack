adc34d4aa39c843b5d410d67f7e97811027ccb9d
youngzx <1216238955@qq.com>
2022-07-02 22:14:31 +0800

    redux数据流理解

--- react/fullstack_music/frontend/src/store/reducer.js
+++ react/fullstack_music/frontend/src/store/reducer.js
@@ -2,8 +2,7 @@
 import { combineReducers } from "redux";
 // store 中央 
 // 地方
-import { reducer as recommendReducer } from
-     '@/pages/Recommend/store/index'
+import { reducer as recommendReducer } from '@/pages/Recommend/store/index'
 import { reducer as playerReducer } from '@/pages/Player/store/index'
 
 export default combineReducers({
