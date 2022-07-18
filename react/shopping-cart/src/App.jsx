4274901a398a335139fcab6e1dcd9e7be5ffe01d
youngzx <1216238955@qq.com>
2022-07-18 16:12:30 +0800

    购物车

--- react/shopping-cart/src/App.jsx
+++ react/shopping-cart/src/App.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux';
-
+import {
+  checkAllGoodsAction,
+  checkGoodsAction,
+  changeGoodsNumAction
+} from './store/actionCreators'
+import './assets/css/shoppingCart.css'
 function App() {
   return (
     <div className="App">
