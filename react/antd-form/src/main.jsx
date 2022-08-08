b50581c03f4927d718382229106a45e5e328e72d
Youngzx <1216238955@qq.com>
2022-08-08 23:02:23 +0800

    form

---
+++ react/antd-form/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
