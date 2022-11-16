b5d2a3bcb45f4590632a82af8f918bd17cf45ca7
Youngzx <1216238955@qq.com>
2022-11-16 22:39:38 +0800

    贪心/动规

---
+++ L_Leetcode/初级算法/1. 数组草稿纸.js
@@ -0,0 +1,8 @@
+// var obj = {}
+// obj[11] = 1
+// console.log(obj)
+
+// const arr = new Array(10).fill(0)
+// const dp = new Array(10).fill(0).map((v) => new Array(2).fill(0))
+// console.log(arr)
+// console.log(dp)
