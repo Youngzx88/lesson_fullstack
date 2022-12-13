0f91bdb6b714b5e895c766e95a0d299d3261bb0c
Youngzx <1216238955@qq.com>
2022-12-13 23:02:49 +0800

    react without cli

---
+++ 学习demo/react/reactwithoutCli/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+const App = (
+    <div className="border">
+        <h1>Manually React</h1>
+    </div>
+)
+ReactDOM.render(App,document.getElementById("root"));
