2bf1bcc19b26dda6bfdd8313f60d3bb83b213426
Youngzx <1216238955@qq.com>
2023-02-09 19:55:03 +0800

    react demo

--- E前端/前端调试/myDebugProject/src/App.jsx
+++ E前端/前端调试/myDebugProject/src/App.jsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-
+import Son from './Son'
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [fatherText, setFatherText] = useState('')
+  const [sonText, setSonText] = useState('')
+  const [value, setValue] = useState(1)
+  
+  const handleValue = () => {
+    setValue(value+1)
+  }
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{border:'1px solid black',width:'300px',height:'200px'}}>
+        <input type="text" value={fatherText} onChange={(e)=>{setFatherText(e.target.value)}}/>
+        <div>子组件输入内容:{sonText}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Son fatherText={fatherText} sonText={sonText} setSonText={setSonText}></Son>
+      <div onClick={handleValue}>值:{value}</div>
     </div>
   )
 }
