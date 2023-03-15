44201c0298cbe939fa2bcc953b9b73054db3c91c
Youngzx <1216238955@qq.com>
2023-03-15 21:46:35 +0800

    trycatch

--- E.frontEnd/JS/trycatch/1.js
+++ E.frontEnd/JS/trycatch/1.js
@@ -1,12 +1,8 @@
 try {
-  var a = "0x112";
   console.log("1")
-  if(a === "0x112"){
-    throw '代码错误'
-  }
-  console.log(a)
+  // console.log(a)
 } catch (error) {
-  console.log(error)
+  console.log(e)//ReferenceError: e is not defined
 } finally {
   console.log("接下来的代码")
 }
