e456a565beac4b0f8bd4d334325ddf51a8704d02
Youngzx <1216238955@qq.com>
2023-08-25 12:00:55 +0800

    babel demo

---
+++ C.frontEnd/babel/babelDemo/lib/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// es6
+var a = 1;
+var b = 2;
+var c = a + b;
+var fn = function fn() {
+  console.log("1");
+};
---
+++ C.frontEnd/babel/babelDemo/src/index.js
@@ -0,0 +1,9 @@
+// es6
+let a = 1
+let b = 2
+let c = a+b
+
+
+const fn = () => {
+  console.log("1")
+}
