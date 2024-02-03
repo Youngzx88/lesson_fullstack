fc90388312daf01ed7cb7785011491bd4f213af5
Youngzx <1216238955@qq.com>
2024-02-04 01:42:24 +0800

    feat:calendar init

---
+++ C.frontEnd/react/react-ui/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Calendar from './components/calendar/index'
+
+function App() {
+  return (
+    <div style={{margin:10}}>
+      <Calendar></Calendar>
+    </div>
+  )
+}
+
+export default App
