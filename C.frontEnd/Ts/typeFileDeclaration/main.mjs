1e1a6ccd68ae1444e610ec3f4b4d1e3610a0fa1e
Youngzx <1216238955@qq.com>
2024-06-24 18:38:45 +0800

    ts

--- C.frontEnd/Ts/typeFileDeclaration/main.mjs
+++
@@ -1,8 +0,0 @@
-import { add,subtract } from "./math.js";
-// const { add, subtract } = require('./math.js');
-
-const res1 = add(1,3)
-console.log("🚀 ~ file: main.js:4 ~ res1:", res1)
-
-const res2 = subtract(1,3)
-console.log("🚀 ~ file: main.js:7 ~ res2:", res2)
--- C.frontEnd/Ts/typeFileDeclaration/math.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'math' {
-  export function add(a: number, b: number): number;
-  export function subtract(a: number, b: number): number;
-}
